db7c2b25069d659dbddafd31f728cc9cebee2a96
Eric Vasilevsky <evasilevsky@gmail.com>
2016-12-24 10:08:14 -0800

    examples set up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,25 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { HeaderComponent } from './header/header.component';
+import { Routes, RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ExampleComponent } from './example/example.component';
+
+const appRoutes: Routes = [
+  { path: 'example', component: ExampleComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    HeaderComponent,
+    PageNotFoundComponent,
+    ExampleComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     FormsModule,
     HttpModule
